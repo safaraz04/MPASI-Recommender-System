f4e04791d3050331b8548cf6d6c1173b321e7c87
safaraz04 <faraz.205026@mhs.its.ac.id>
2024-12-09 23:35:50 +0700

    add all files

---
+++ Model/recipe_sim_filtering.py
@@ -0,0 +1,62 @@
+import numpy as np
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+# from sklearn.metrics.pairwise import cosine_similarity
+
+
+df = pd.read_excel("Concept_Data Resep_Updated.xlsx")
+df = df.replace(np.nan,"")
+df['umur_resep'] = df['umur_resep'].astype(str)
+df['umur_resep'] = df['umur_resep'].apply(lambda x: list(map(int, x.split(','))))
+df_clean = df.copy()
+df_clean[['bahan_resep','bahan_alergen']] = df_clean[['bahan_resep','bahan_alergen']].replace(to_replace=r'[^\w\s]', value=' ', regex=True)
+data = pd.DataFrame(df_clean['bahan_resep'].astype(str)+ ' '+ df_clean['bahan_alergen'].astype(str),columns=['bahan_resep_dan_alergen'])
+
+
+# turunan_alergi_input = {
+#     'susu': ['yogurt','krim','mentega','whey','kasein','keju'],
+#     'telur': ['mayonnaise','meringue'],
+#     'ikan': ['minyak ikan','gelatin ikan'],
+#     'gluten' : ['roti','pasta','tepung','mie'],
+#     'udang': ['ebi','petis','terasi','kerupuk udang','koya'],
+#     'kerang': ['saus tiram'],
+#     'kacang': ['minyak wijen', 'minyak kedelai', 'mustard', 'tahu', 'tofu', 'tempe', 'kerupuk kedelai', 'bumbu kacang', 'kecap']
+# }
+
+
+# def expand_user_input(user_input, turunan_alergi_input):
+#     parent_input = [user_input]
+#     if user_input in turunan_alergi_input:
+#         parent_input.extend(turunan_alergi_input[user_input])
+#     return parent_input
+
+def find_recipes_sim(data_matrix, user_input, min_similarity = 0):
+    vectorizer = CountVectorizer()
+    data_matrix = vectorizer.fit_transform(data['bahan_resep_dan_alergen'])
+    user_matrix = vectorizer.transform([user_input])
+    data_matrix_sets = [row.indices for row in data_matrix]
+    user_matrix_sets = [row.indices for row in user_matrix][0]
+
+    similarities = []
+    for row in data_matrix_sets:
+        intersection = len(list(set(user_matrix_sets).intersection(row)))
+        similarity = 2*float(intersection)/(len(set(user_matrix_sets)) + len(set(row)))
+        similarities.append(similarity)
+    top_n = 10
+    top_recipes = [(df.loc[i, ['nama_resep', 'bahan_resep', 'bahan_alergen', 'umur_resep']], similarities[i]) 
+                   for i in np.argsort(similarities) if similarities[i] == min_similarity]
+
+    return top_recipes
+
+    # top_recipes = []
+    # for i in top_indices:
+    #     age_list = list(map(int, str(df.iloc[i]['umur_resep']).split(',')))
+        
+    #     if any(age <= user_age for age in age_list) and similarities[0, i] >= min_similarity:
+    #         top_recipes.append((df[['nama_resep', 'bahan_resep', 'bahan_alergen', 'umur_resep']].iloc[i], similarities[0, i]))
+    #         if len(top_recipes) >= top_n:
+    #             break
+                        
+    # top_recipes = [(df[['nama_resep','bahan_resep','bahan_alergen','umur_resep']].iloc[i], similarities[0,i]) for i in top_indices if similarities [0,i] <= min_similarity][:top_n]
+
+    # return top_recipes
--- Model/recommender-app.py
+++ Model/recommender-app.py
@@ -1,6 +1,12 @@
+# ---------------------------- library -----------------------------------------
+
 import streamlit as st
 import numpy as np
 import pandas as pd
+import re
+from recipe_sim_filtering import find_recipes_sim
+# ---------------------------- library -----------------------------------------
+
 
 html_temp = """
 <style>
@@ -14,10 +20,86 @@ st.set_page_config(
 )
 
 st.title("Allergy-based Baby's Food Recommendation")
-st.caption('Faraz Nurdini | 5026201007')
 
-user_name = st.text_input("Enter your baby's name:")
-st.text("Select one or many allergen:")
-user_input = st.checkbox("Susu",label_visibility='visible')
+user_name = st.text_input("Silahkan masukkan nama bayi anda:")
+user_age = st.slider("Umur bayi (Dalam Bulan):",6,12)
+st.text("Pilih satu alergen atau lebih:")
+
+alergen_utama = ['susu','ikan','gluten','kerang','kacang','udang','telur']
+turunan_map = {
+    'susu': ['yogurt', 'krim', 'mentega', 'whey', 'kasein', 'keju'],
+    'ikan': ['minyak ikan', 'gelatin ikan'],
+    'gluten': ['roti', 'pasta', 'tepung', 'mie'],
+    'kerang': ['saus tiram'],
+    'kacang': ['minyak wijen', 'minyak kedelai', 'mustard', 'tahu', 'tofu', 'tempe', 
+               'kerupuk kedelai', 'bumbu kacang', 'kecap'],
+    'udang': ['ebi', 'petis', 'terasi', 'kerupuk udang', 'koya'],
+    'telur': ['mayonnaise', 'meringue']
+}
+
+col1,col2, col3, col4 = st.columns(4)
+user_input_values = []
+
+
+for i in alergen_utama:
+    if i not in st.session_state:
+        st.session_state[i] = False
+
+for i in alergen_utama:
+    if st.session_state[i] == True :
+         for x in turunan_map.get(i, []):
+              st.session_state[x] = True
+
+def delete_child() :
+    for i in alergen_utama: 
+        for x in turunan_map.get(i, []): 
+            st.session_state[x] = False
+               
+
+
+for i in alergen_utama:
+     with col1:
+          if st.checkbox(i, value=st.session_state[i], on_change=delete_child ,key=i):
+               user_input_values.append(i)
+
+columns = [col2, col3, col4] 
+for idx, (main_allergen, derivatives) in enumerate(turunan_map.items()):
+    col_idx = idx % len(columns)  
+    with columns[col_idx]:
+        for derivative in derivatives:
+            if st.checkbox(derivative, value=st.session_state.get(derivative, False), key=derivative):
+                user_input_values.append(derivative)
+
+
+user_input = ' '.join(user_input_values)
+
+
+if user_input_values:
+    st.write("Anda memilih:", ", ".join(user_input_values))
+else:
+    st.write("Tidak ada alergi yang terpilih.")
+
+st.text('')
+
+df = pd.read_excel("Concept_Data Resep_Updated.xlsx")
+
+if st.button("Recommend Recipe"):
+        st.subheader(f"Recommended Recipe for {user_name}")
+        recommended_recipe = find_recipes_sim(df, user_input)
+        recipe_result = pd.DataFrame( 
+            [{**recipe[0].to_dict(), "similarity_score": recipe[1]} for recipe in recommended_recipe]
+            )
+        top_n = 10
+        filtered_data = recipe_result[recipe_result['umur_resep'].apply(lambda x: user_age in x )]
+        filtered_pd = pd.DataFrame(filtered_data)[:top_n]
+
+        for index,row in filtered_pd.iterrows():
+            with st.expander(row["nama_resep"]):
+                st.write(f"**Nama Resep:** {row["nama_resep"]}")
+                st.write(f"**Bahan Resep:** {row["bahan_resep"]}")
+                st.write(f"**Bahan Alergen:** {row["bahan_alergen"]}")
+                st.write(f"**Umur Resep:** {', '.join(map(str, row['umur_resep']))}")
+                st.write(f"**Similarity Score:** {row["similarity_score"]}")
+
 
 
